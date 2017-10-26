02c3189c3e472af4a00581ec0a5ed6c9af2fa571
linda5000 <linda5000@163.com>
2017-10-26 16:31:21 +0800

    1

--- log_20171012.py
+++ log_20171012.py
@@ -21,3 +21,19 @@ logger.warning("这个是我们的第三条日志")
 logging.info("这个是我们的第1条日志")
 logging.debug("这个是我们的第2条日志")
 logging.warning("这个是我们的第3条日志")
+
+
+
+
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+                    datefmt='%a, %d %b %Y %H:%M:%S',
+                    filename='test.log',
+                    filemode='w')
+
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+console.setFormatter(formatter)
+logging.getLogger('').addHandler(console)
